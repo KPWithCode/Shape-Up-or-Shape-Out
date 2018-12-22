b5ed221c872b6fdd04492b443527c81631ed8174
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2018-12-22 17:38:23 -0500

    Added div and div class

--- shapeout.js
+++ shapeout.js
@@ -2,5 +2,7 @@ class Shape {
     constructor(height,width)   {
         this.height = height;
         this.width = width;
+        this.div = document.createElement('div');
+        this.div.classList.add('shape');
     }
 }
